3cd2399c9bd02de45a58aeccbc87836084790f84
Artem Filatov <filatovartm@gmail.com>
2018-03-19 19:39:02 +0300

    add multiple add and removed attention matching

--- cache_numpy.py
+++ cache_numpy.py
@@ -6,30 +6,38 @@ class LRUCache:
         self.lru_state_ = []
         self.max_size_ = max_size
 
-    def Query(self, attention_vector):
-        result = np.zeros(len(attention_vector))
+    def Query(self, matching_vector):
+        result = np.zeros(len(matching_vector))
 
         sum = 0
-        for att_vec, state_vec in self.container_.values():
-            sum += np.exp(np.dot(attention_vector, att_vec))
+        for state_vec in self.container_.values():
+            sum += np.exp(np.dot(matching_vector, state_vec))
 
-        for att_vec, state_vec in self.container_.values():
-            result += state_vec * np.exp(np.dot(att_vec, attention_vector))
+        for state_vec in self.container_.values():
+            result += state_vec * np.exp(np.dot(state_vec, matching_vector))
 
         return result / sum
 
-    def Add(self, key, attention_vector, state_vector):
-        if len(self.container_) > self.max_size_:
+    def Add(self, key, state_vector):
+        if len(self.container_) == self.max_size_:
             lru_key = self.lru_state_.pop()
             self.container_.pop(lru_key)
 
         if key not in self.container_:
-            self.container_[key] = [attention_vector, state_vector]
+            self.container_[key] = state_vector
         else:
-            old_att, old_state = self.container_[key]
-            self.container_[key] = [(old_att + attention_vector) / 2, (old_state + state_vector) / 2]
+            old_state = self.container_[key]
+            self.container_[key] = (old_state + state_vector) / 2
             self.lru_state_.remove(key)
 
         self.lru_state_.insert(0, key)
-        return attention_vector
+        return state_vector
+
+    def AddMultipleEntries(self, keys, state_vectors):
+        keys = keys.ravel()
+        reshaped_states = state_vectors.reshape(len(keys), -1)
+        for key, state_vector in zip(keys, reshaped_states):
+            self.Add(key, state_vector)
+
+        return state_vectors
 
